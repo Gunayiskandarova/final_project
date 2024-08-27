409c909a3e68fe0f47ab63f44d1803b25ac74afe
Gunayiskandarova <gunayiskandarova29@gmail.com>
2024-08-27 22:08:16 +0400

    main_console

---
+++ console/console.py
@@ -0,0 +1,25 @@
+def main():
+    print("Welcome to FLIGHT")
+    print("Please select option:")
+    print("Press 1 for add new flight")
+    print("Press 2 for add new booking")
+    print("Press 3 for display all flight")
+    print("Press 4 for exit")
+ choise = input ("Please choice your option: ")
+ while choise <= 5:
+     if choise==1:
+#          addnew flight
+     elif choise==2:
+#         add new booking
+     elif choise==3:
+#          display all flight
+     elif choise==4:
+         break
+     else: print("Please choise correct vaeriant:")
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from console.console import main
+if main="main":
+    main()
+
