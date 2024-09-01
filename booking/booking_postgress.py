90b38aaa7609d7715dd75840171fa9a466ba1c8b
Gunayiskandarova <gunayiskandarova29@gmail.com>
2024-09-01 19:46:01 +0400

    marge_sort_in_booking_postgress

--- booking/booking_postgress.py
+++ booking/booking_postgress.py
@@ -13,6 +13,7 @@ def save_booking_details(booking_details, conn):
             booking_details["count_seat"]
         ))
         conn.commit()
+        # update_booking_save()
         print("Booking details saved successfully")
     except Exception as e:
         print(f"Error: {e}")
@@ -55,6 +56,19 @@ def find_all_booking(conn):
       print(row)
   except Exception as e:
     print(f"Error: {e}")
+  
+def find_booking_by_id(flight_id, conn):
+  insert_query = '''
+  SELECT * FROM booking
+  WHERE id = %s
+  '''
+  try:
+    with conn.cursor() as curr:
+      curr.execute(insert_query, (flight_id,))
+      result = curr.fetchone() 
+      print(result)
+  except Exception as e:
+    print(f"Error: {e}")
 
 
 def delete_booking(booking_id,conn):
@@ -66,11 +80,152 @@ def delete_booking(booking_id,conn):
   try:
     cursor.execute(delete_query, (booking_id,))
     conn.commit()
+    # update_seats_delete()
     print(f"FLİGHT ID {booking_id} SUCCESSFULLY DELETE!")
-
-
   except Exception as e:
     conn.rollback()
     print(f"Error deleting booking: {e}")
   finally:
     conn.close()
+
+
+from datetime import datetime
+
+def merge_sort_by_date(dates, conn):
+  cursor = conn.cursor()
+  try:
+    cursor.execute(dates,conn)
+    if len(dates) <= 1:
+     return dates
+
+    middle = len(dates) // 2
+    left_list = merge_sort_by_date(dates[:middle])
+    right_list = merge_sort_by_date(dates[middle:])
+
+    return merge_dates(left_list, right_list)
+  except Exception as e:
+    conn.rollback()
+    print(f"Error deleting booking: {e}")
+  finally:
+    conn.close()
+
+def merge_dates(left, right, conn):
+  sorted_dates = []
+  i = j = 0
+  cursor = conn.cursor()
+  cursor.execute(left,right, conn)
+
+  while i < len(left) and j < len(right):
+       if left[i] < right[j]:
+         sorted_dates.append(left[i])
+         i += 1
+       else:
+         sorted_dates.append(right[j])
+         j += 1
+
+  sorted_dates.extend(left[i:])
+  sorted_dates.extend(right[j:])
+  return sorted_dates
+
+
+dates = [
+  datetime ("YYYY-MM-DD"),
+  datetime("YYYY-MM-DD"),
+  datetime("YYYY-MM-DD"),
+  datetime("YYYY-MM-DD"),
+  datetime("YYYY-MM-DD")
+]
+
+sorted_dates = merge_sort_by_date(int(dates))
+for date in sorted_dates:
+       print(date.strftime('%Y-%M-%D'))     #"YYYY-MM-DD" string formata çevirmək üçün
+
+def delete_booking(booking_id,conn):
+ delete_query = """
+ DELETE FROM booking
+ WHERE id = %s;
+ """
+ cursor = conn.cursor()
+ try:
+  cursor.execute(delete_query, (booking_id,))
+  a = conn.commit()
+  if a == True:
+    # update_delete_bookings()
+    print(f"FLİGHT ID {booking_id} SUCCESSFULLY DELETE!")
+ except Exception as e:
+  conn.rollback()
+  print(f"Error deleting booking: {e}")
+ finally:
+  conn.close()
+
+
+
+
+
+  def save_booking(booking_details, conn):
+      insert_query = """
+    INSERT INTO booking (id, passenger_name, flight_id, seat_count)
+    VALUES (%s, %s, %s, %s)
+        """
+
+  cursor = conn.cursor()
+  try:
+    cursor.execute(insert_query, (
+      booking_details["id"],
+      booking_details["passenger_name"],
+      booking_details["flight_id"],
+      booking_details["seat_count"]
+      # flight_id,
+      # seat_count,
+      ))
+    conn.commit()
+    update_save_bookings(flight_id, seat_count, conn)
+    print("Saved successfully!")
+  except Exception as e:
+    conn.rollback()
+    print(f"Error: {e}")
+  finally:
+   cursor.close()
+
+def update_save_bookings(flight_id, seat_count, conn):
+  update_query = """UPDATE booking SET seat_count = seat_count - %s
+  WHERE flight_id = %s"""
+  cursor = conn.cursor()
+  try:
+    cursor.execute(update_query, (flight_id, seat_count))
+    print("Count of seats updated successfully!")
+    conn.commit()
+  except Exception as e:
+    conn.rollback()
+    print(f"Error: {e}")
+
+
+def update_delete_bookings(flight_id, seat_count, conn):
+  insert_query = """UPDATE booking SET seat_count = seat_count + %s
+  WHERE flight_id = %s"""
+  cursor = conn.cursor()
+  try:
+    cursor.execute(insert_query, (flight_id, seat_count))
+    print("Count of seats updated successfully!")
+    conn.commit()
+  except Exception as e:
+    conn.rollback()
+    print(f"Error: {e}")
+  
+
+def delete_booking(booking_id, flight_id, seat_count, conn):
+ delete_query = """
+ DELETE FROM booking
+ WHERE id = %s;
+ """
+ cursor = conn.cursor()
+ try:
+  cursor.execute(delete_query, (booking_id,))
+  conn.commit()
+  update_delete_bookings(flight_id, seat_count, conn)
+  print(f"FLİGHT ID {booking_id} SUCCESSFULLY DELETE!")
+ except Exception as e:
+  conn.rollback()
+  print(f"Error deleting booking: {e}")
+ finally:
+  conn.close()
--- console/console.py
+++ console/console.py
@@ -1,6 +1,6 @@
 from booking.booking_operation import create_booking_details,booking_update_details
-from booking.booking_postgress import save_booking_details,update_booking, find_all_booking, delete_booking
-from flight.flight_operation import create_flight_details, flight_update_details, flight_delete_details
+from booking.booking_postgress import save_booking_details,update_booking, find_all_booking, delete_booking, find_booking_by_id
+from flight.flight_operation import create_flight_details, flight_update_details 
 from flight.flight_postgres import save_flight_details, uptade_flight, find_flight_by_id, delete_flight,find_all_flights, find_flights_by_origin
 from config.connect_postgres import connect_to_database
 
@@ -9,8 +9,15 @@ def console_menu():
     print("Please select option:")
     print("Press 1 for add new flight")
     print("Press 2 for add new booking")
-    print("Press 3 for display all flight")
-    print("Press 4 for exit")
+    print("Press 3 for find flight by id")
+    print("Press 4 for find all booking")
+    print("Press 5 for booking update") 
+    print("Press 6 for flight update") 
+    print("Press 7 for delete flight")  
+    print("Press 8 for delete booking") 
+    print("Press 9 for find all flight")   
+    print("Press 10 for find flight by origin")    
+    print("Press 11 for find booking by id")     
     choise = input("Please choice your option: ")
     return choise
 
@@ -28,19 +35,19 @@ def main():
         elif choise=="2":
             booking_details=create_booking_details()
             save_booking_details(booking_details,postgres)
-        elif choise=="3":
-            uptade_details = flight_update_details()
-            update_for_id = int(input("Enter update ID: "))
-            uptade_flight(update_for_id, uptade_details, postgres)
+        elif choise == "3":
+            flight_id = int(input("Enter id: "))
+            find_flight_by_id(flight_id, postgres)
         elif choise== "4":
             find_all_booking(postgres)
         elif choise == "5":
             update_details = booking_update_details()
             update_for_id = int(input("Enter update id: "))
             update_booking(update_for_id, update_details, postgres)
-        elif choise == "6":
-            flight_id = int(input("Enter id: "))
-            find_flight_by_id(flight_id, postgres)
+        elif choise=="6":
+            uptade_details = flight_update_details()
+            update_for_id = int(input("Enter update ID: "))
+            uptade_flight(update_for_id, uptade_details, postgres)
         elif choise == "7":
             flight_id= int(input("Enter id: "))
             delete_flight(flight_id, postgres)
@@ -48,13 +55,15 @@ def main():
             booking_id = int(input("Enter id: "))
             delete_booking(booking_id,postgres)
         elif choise == "9":
-            flight_id = int(input("Enter id: "))
-            find_flight_by_id(flight_id,postgres)
+             find_all_flights()
         elif choise == "10":
             origin = input("Enter origin city: ")
             find_flights_by_origin(origin, postgres)
+        elif choise =="11":
+            flight_id = int(input("Enter id: "))
+            find_booking_by_id(flight_id, postgres)            
         else:
-            print("Please choise correct vaeriant:")
+            print("Please choise correct variant:")
 
 
 
